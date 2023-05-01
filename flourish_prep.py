b472ce08753ff02689c774f532505f368646e656
Yuv local <billiondollarone07@gmail.com>
2023-05-01 22:04:53 +0530

    - Finished the preparation script for Flourish studio - Cleaner data processing in the main.py module

--- flourish_prep.py
+++ flourish_prep.py
@@ -28,16 +28,25 @@ for line in content[1:]:
 flourish_headers = ['names']
 
 for date_ in date_count:
-    # Fa
     flourish_headers.append(date_)
     for name_ in names:
         if name_ in date_count[date_]:
-            # write here
-            break
+            names[name_].append(str(int(names[name_][-1] if len(names[name_]) > 0 else str(0)) + date_count[date_][name_]))
+        else:
+            names[name_].append(names[name_][-1] if len(names[name_]) > 0 else str(0))
 
+out_csv_rows = list()
 
-
+for row_head in names:
+    out_csv_rows.append(','.join([row_head] + names[row_head]))
+    
 
 out = open(f'./output/{FLOURISH_JSON_OUT_FILE}', 'w')
 out.write(json.dumps(date_count))
 out.close()
+print('Saved the JSON file.')
+
+out_csv = open(f"./output/{FLOURISH_OUT_FILE}", 'w')
+out_csv.write('\n'.join([','.join(flourish_headers + list(date_count.keys()))] + out_csv_rows))
+out_csv.close()
+print('Saved the CSV file.')
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ content = f.readlines()
 f.close()
 headers = ['date', 'time', 'user', 'media']
 message_count = 0
+
 for line in content:
     l = line.strip()
     date_re = r"\d\d/\d\d/\d\d, \d+:\d\d:\d\d [A|P]M"
@@ -17,10 +18,10 @@ for line in content:
         media_re = r"[GIF|image|audio|sticker] omitted"
         media_match = re.search(media_re, l)
 
-        message_date = date_match.group()
-        name = l[l.index(message_date) + len(message_date) + 2:].split(':')[0].rstrip().replace('\u202a', '').replace('\u202c', '').replace('\u00a0', '')
+        message_date = str(date_match.group())
+        name = l[l.index(message_date) + len(message_date) + 1:].replace('\u202a', '').replace('\u202c', '').replace('\u00a0', ' ').split(':')[0].strip()
         hasMedia = str(int(bool(media_match)))
-        data.append(','.join([message_date, name, hasMedia]))
+        data.append(','.join(message_date.split(', ') + [name, hasMedia]))
 
 
 print(f"Number of messages: {message_count}")
